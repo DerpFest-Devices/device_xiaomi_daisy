f0371b94a3ddd5226d25545008e300016d6698c3
Michael Bestas <mkbestas@lineageos.org>
2018-05-14 22:15:24 +0300

    daisy: Don't build tinymix
    * This tool can be built manually for debugging cases,
      there is no need to ship it in production
    
    Change-Id: I9ab8b076b4bede70c368c198960ceabc68dad65b

--- device.mk
+++ device.mk
@@ -106,8 +106,7 @@ PRODUCT_PACKAGES += \
     libqcomvisualizer \
     libqcomvoiceprocessing \
     libqcompostprocbundle \
-    libvolumelistener \
-    tinymix
+    libvolumelistener
 
 # A2DP
 PRODUCT_PACKAGES += \
