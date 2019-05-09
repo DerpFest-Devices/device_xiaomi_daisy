0b58d05d31666d916597615dde57acd1b3cd38bc
steadfasterX <steadfasterX@gmail.com>
2019-05-09 18:51:00 +0800

    daisy: prop: Fix Graphic Glitches
    on skiagl in some rare situations graphic bugs occur
    so we enforce opengl for now
    
     * adb shell setprop debug.hwui.renderer skiagl
     * adb shell setprop debug.hwui.renderer skiavk
       (buggy here as FCs on some apps)
     * adb shell setprop debug.hwui.renderer opengl
       (can be named anything as if the both other doesnt match if fallback to opengl)
    
    Change-Id: I73edc7e380e869e0241d976873322786bac5b98c
    Signed-off-by: TogoFire <italomellopereira@gmail.com>

--- prop.mk
+++ prop.mk
@@ -101,7 +101,8 @@ ro.vendor.display.cabl=2 \
 sdm.debug.disable_skip_validate=1 \
 vendor.display.disable_skip_validate=1 \
 vendor.display.enable_default_color_mode=0 \
-vendor.gralloc.enable_fb_ubwc=1
+vendor.gralloc.enable_fb_ubwc=1 \
+debug.hwui.renderer=opengl
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
