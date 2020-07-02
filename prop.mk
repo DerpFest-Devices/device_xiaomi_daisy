2d111c2506e6b8c21b6a31b3542a0b09bd9f1d3e
jro1979oliver <jroliveira.oliveira301@gmail.com>
2020-07-02 14:21:38 +0000

    daisy: prop: Add SurfaceFlinger props
    Change-Id: Ifb1cbb9d012f684d99c318417e14f1a4f1e59120
    
    Signed-off-by: TogoFire <italomellopereira@gmail.com>
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- prop.mk
+++ prop.mk
@@ -203,6 +203,14 @@ persist.vendor.vt.supported=1 \
 persist.vendor.sys.cnd.iwlan=1 \
 persist.vendor.cne.logging.qxdm=3974
 
+# SurfaceFlinger
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+ro.surface_flinger.protected_contents=true \
+ro.surface_flinger.vsync_event_phase_offset_ns=4000000 \
+ro.surface_flinger.vsync_sf_event_phase_offset_ns=8000000 \
+ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+ro.surface_flinger.max_virtual_display_dimension=4096
+
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.timed.enable=true \
