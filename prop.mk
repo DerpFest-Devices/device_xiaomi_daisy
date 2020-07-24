6bfdbf4ea61acbcc4e6b08e636a08e2027b3410e
TogoFire <italomellopereira@gmail.com>
2020-07-24 11:16:02 -0300

    Revert "msm8953-common: props: Enable USB debugging at boot"
    This reverts commit 1a7cb27d25096b14ff6e2f1e19ceb4979f1517a2.

--- prop.mk
+++ prop.mk
@@ -233,13 +233,6 @@ net.tcp.2g_init_rwnd=10
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.vendor.usb.config.extra=none
 
-# USB debugging
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.sys.usb.config=mtp,adb \
-ro.adb.secure=0 \
-ro.secure=0 \
-ro.debuggable=1
-
 # Wifi
 PRODUCT_PROPERTY_OVERRIDES += \
 wifi.interface=wlan0
