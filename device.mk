1c621dde57caa5f24c352296221a198ec40833e7
lyceris-chan <ghostdrain@outlook.com>
2020-08-03 17:22:53 +0200

    daisy: drop lineage overlays

--- device.mk
+++ device.mk
@@ -26,7 +26,6 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
