f382c2041f2af22edb3fb17f1625d4761c84ed94
lupesoltec <dev@lupesoltec.com.br>
2020-08-20 19:01:47 -0400

    daisy: parts: Introduce HAL3|HAL1 Switch
    Introducing a HAL switch so users can easily swap between HAL1 and HAL3 without root and or having to reboot
    
    @TogoFire Adapted: for XiaomiParts
    @lupesoltec Adapted: for "Lineage" parts
    
    This implementation is based / inspired by:
    
    https://github.com/TogoFire/dt_xiaomi_daisy/commit/5aadeafd116bbcc06adf50191dd54c3ce3295d36
    
    https://github.com/TogoFire/dt_xiaomi_daisy/commit/51b108b1e55dc82d51f5d6005750031b2c0e8127
    
    https://github.com/TogoFire/dt_xiaomi_daisy/commit/3cb83dc9dda740673a7abdc2e3d36faf13511481
    
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>
    Co-Authored-By: Togo 77 <togofire@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -348,7 +348,9 @@ PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
     init.qcom.post_boot.sh \
     move_time_data.sh \
-    move_wifi_data.sh
+    move_wifi_data.sh \
+    init.camera.rc \
+    init.camera.sh
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -128,3 +128,21 @@ LOCAL_SRC_FILES       := sbin/chargeonlymode
 LOCAL_MODULE_TAGS     := optional
 LOCAL_MODULE_CLASS    := EXECUTABLES
 include $(BUILD_PREBUILT)
+
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.camera.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.camera.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.camera.sh
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.camera.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_EXECUTABLES)
+include $(BUILD_PREBUILT)
+
