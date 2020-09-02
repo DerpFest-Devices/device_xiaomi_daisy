137f35a897fab22f9d460f70944a2fb2c9558cd9
KenHV <kenhv@null.net>
2020-09-02 17:07:36 +0530

    daisy: prop: use skiagl to render ui
    Signed-off-by: KenHV <kenhv@null.net>

--- prop.mk
+++ prop.mk
@@ -84,6 +84,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 debug.egl.hw=0 \
 debug.enable.sglscale=1 \
 debug.gralloc.enable_fb_ubwc=1 \
+debug.hwui.renderer=skiagl \
 debug.mdpcomp.logs=0 \
 debug.sf.hw=0 \
 debug.sf.latch_unsignaled=1 \
