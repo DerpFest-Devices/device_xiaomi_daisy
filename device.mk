8cc82bba8b9423d425973cb91fb0a4240f7d80d8
poad42 <me@adhityamohan.in>
2020-09-18 05:21:42 +0000

    msm8953-common: Build SignApk

--- device.mk
+++ device.mk
@@ -416,6 +416,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     device/xiaomi/daisy
 
+# Signapk
+PRODUCT_HOST_PACKAGES += \
+    signapk
+
 # TextClassifier smart selection model files
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
