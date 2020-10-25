0f88f652b7d1f20ad954cbf16a1c64b9a4811a14
Han Wang <416810799@qq.com>
2020-10-25 13:00:58 +0100

    daisy: Drop Force enable mobile network settings v2
     * This is removed and on by default on 11.
    
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- prop.mk
+++ prop.mk
@@ -282,7 +282,6 @@ rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
 ro.telephony.iwlan_operation_mode=legacy \
 ro.telephony.call_ring.multiple=false \
 ro.telephony.default_network=22,20 \
-persist.sys.fflag.override.settings_network_and_internet_v2=true \
 service.qti.ims.enabled=1 \
 persist.vendor.vt.supported=1 \
 persist.vendor.sys.cnd.iwlan=1 \
