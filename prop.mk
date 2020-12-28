9c45a01c983d02b0af4ba6b327313cf4160623ae
Adithya R <gh0strider.2k18.reborn@gmail.com>
2020-12-28 15:45:18 +0600

    daisy: disable hwc vds in surface flinger
    * Fixes jittery ui during screen recorder until vendor R realease is available
    
    Change-Id: I5f27374eb6a9fce26a3f17073e65fe31dcfc8e54

--- prop.mk
+++ prop.mk
@@ -117,7 +117,6 @@ debug.mdpcomp.logs=0 \
 debug.performance.tuning=1 \
 debug.sf.disable_backpressure=1 \
 debug.sf.disable_hwc=0 \
-debug.sf.enable_hwc_vds=1 \
 debug.sf.gpu_comp_tiling=1 \
 debug.sf.hw=1 \
 debug.sf.latch_unsignaled=1 \
