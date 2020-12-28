7569abceeb9e9a475204b744981c2ddb1f562dd8
ZunayedDihan <zunayeddihan@gmail.com>
2020-12-28 15:41:22 +0600

    Revert "Daisy: Use Pixel charger animation"
    This reverts commit 281fc115df9b49a7aa4a9181957a05367e7468a5.

--- device.mk
+++ device.mk
@@ -552,12 +552,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
 
-# Charging
-PRODUCT_PACKAGES += \
-    charger_res_images \
-    product_charger_res_images
-
-
 # Wi-Fi Display
 PRODUCT_PACKAGES += \
     libaacwrapper \
