202aba7f18ff0322d9f3f7e80bd976157915fea5
GeoPD <geoemmanuelpd2001@gmail.com>
2021-03-10 00:51:21 +0600

    daisy: Import basic USB HAL 1.0
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>
    Signed-off-by: lupesoltec <dev@lupesoltec.com.br>

--- device.mk
+++ device.mk
@@ -498,7 +498,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB HAL
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.daisy
 
 # Vendor SPL
 VENDOR_SECURITY_PATCH = $(PLATFORM_SECURITY_PATCH)
