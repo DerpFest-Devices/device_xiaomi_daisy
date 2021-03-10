ef8367b191a141b179a9db6ab29de5aa110398bd
ZunayedDihan <zunayeddihan@gmail.com>
2021-03-10 23:47:58 +0600

    daisy: build android.hardware.camera.device@3.4.so
    E ims_media_jni: Error loading library lib-imsvt.so: dlopen failed: library "android.hardware.camera.device@3.4.so" not found: needed by /system/product/lib64/lib-imsvideocodec.so in namespace classloader-namespace-shared

--- device.mk
+++ device.mk
@@ -185,6 +185,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service \
     android.hardware.camera.provider@2.5 \
     vendor.qti.hardware.camera.device@1.0.vendor \
+    android.hardware.camera.device@3.4.so \
     libdng_sdk.vend \
     Snap
 
