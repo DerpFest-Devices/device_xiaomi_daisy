f813fffd007953a3afca9e5d997cd2ea8aa0b186
Ondra3211 <ondra.niesner@seznam.cz>
2021-04-24 14:39:38 +0600

    daisy: Change active state of HAL switch tile

--- parts/src/org/lineageos/settings/camera/CameraTileService.java
+++ parts/src/org/lineageos/settings/camera/CameraTileService.java
@@ -7,17 +7,21 @@ import org.lineageos.settings.R;
 public class CameraTileService extends TileService {
 
     private CameraUtils mCameraUtils;
-	
+
     public static final String CAMERA_SYSTEM_PROPERTY = "persist.camera.profile";
 
     @Override
     public void onStartListening() {
         int currentState = mCameraUtils.getintProp(CAMERA_SYSTEM_PROPERTY, 0);
-
         Tile tile = getQsTile();
-        tile.setState(Tile.STATE_ACTIVE);
-        tile.setLabel(getResources().getStringArray(R.array.camera_entries)[currentState]);
 
+        if (currentState == 0) {
+            tile.setState(Tile.STATE_INACTIVE);
+        } else {
+            tile.setState(Tile.STATE_ACTIVE);
+        }
+
+        tile.setLabel(getResources().getStringArray(R.array.camera_entries)[currentState]);
         tile.updateTile();
         super.onStartListening();
     }
@@ -25,15 +29,17 @@ public class CameraTileService extends TileService {
     @Override
     public void onClick() {
         int currentState = mCameraUtils.getintProp(CAMERA_SYSTEM_PROPERTY, 0);
+        Tile tile = getQsTile();
 
         int nextState;
         if (currentState == 1) {
+            tile.setState(Tile.STATE_INACTIVE);
             nextState = 0;
         } else {
+            tile.setState(Tile.STATE_ACTIVE);
             nextState = currentState + 1;
         }
 
-        Tile tile = getQsTile();
         mCameraUtils.setintProp(CAMERA_SYSTEM_PROPERTY, nextState);
         tile.setLabel(getResources().getStringArray(R.array.camera_entries)[nextState]);
 
