9ee6b5ce3b23f18b65ccf500253977dc6981effe
FrosT2k5 <androygaming@gmail.com>
2021-05-05 14:25:02 +0530

    daisy: post-boot.sh: Set swappiness to 75 instead of 100

--- rootdir/etc/init.qcom.post_boot.sh
+++ rootdir/etc/init.qcom.post_boot.sh
@@ -332,7 +332,7 @@ if [ "$ProductName" == "msmnile" ]; then
       configure_zram_parameters
       configure_read_ahead_kb_values
       echo 0 > /proc/sys/vm/page-cluster
-      echo 100 > /proc/sys/vm/swappiness
+      echo 75 > /proc/sys/vm/swappiness
 else
     arch_type=`uname -m`
     MemTotalStr=`cat /proc/meminfo | grep MemTotal`
