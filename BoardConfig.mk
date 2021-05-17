357edb95c630eab365f44420b4ebeddf88a66af9
GeoPD <geoemmanuelpd2001@gmail.com>
2021-05-17 18:23:32 +0000

    daisy: : Compile kernel with clang & ship with revvz kernel
    *Use latest clang 11.0.2 as per android-11.0.0_r36
    
    This reverts commit 5627454e872052cbcccadda1b2c6a390c0fa6ec7.
    
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -166,14 +166,10 @@ BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
-TARGET_KERNEL_CONFIG := daisy_defconfig
+TARGET_KERNEL_CONFIG := sakura_defconfig
 TARGET_KERNEL_VERSION := 4.9
-
-# Proton
 TARGET_KERNEL_CLANG_COMPILE := true
-#TARGET_KERNEL_CLANG_VERSION := proton
-#KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-proton/bin
-#TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
+TARGET_KERNEL_CLANG_VERSION := r383902b1
 
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
