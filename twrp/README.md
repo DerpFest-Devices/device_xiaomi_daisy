1b784089c24bf883cdaefe6e65ff720cf80a4a1f
Hotorou <zunayeddihan@gmail.com>
2021-07-02 02:52:20 +0000

    daisy: remove pre built twrp

--- twrp/README.md
+++
@@ -1,1 +0,0 @@
- Pick before building <https://gerrit.aicp-rom.com/c/AICP/build/+/94064>
--- twrp/twrp.mk
+++
@@ -1,5 +0,0 @@
-TWRP_PATH := device/xiaomi/daisy/twrp
-
-TARGET_PREBUILT_RECOVERY_RAMDISK_CPIO := $(TWRP_PATH)/ramdisk-recovery.cpio
-#TARGET_PREBUILT_RECOVERY_RAMDISK_IMG := $(TWRP_PATH)/ramdisk-recovery.img
-#TARGET_PREBUILT_RECOVERY_RAMDISK := $(TWRP_PATH)/ramdisk-recovery.zip 
