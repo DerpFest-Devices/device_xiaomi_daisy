d06ba47d8302be0d8e14381b87fc3e3a83d4d89b
Steven Moreland <smoreland@google.com>
2021-07-15 15:48:56 +0000

    daisy: Only include bitness of hal impls that is used
    Bug: 73829928
    Test: boot, check directories, no errors during boot
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>
    Signed-off-by: ZunayedDihan <zunayeddihan@gmail.com>

--- device.mk
+++ device.mk
@@ -183,10 +183,10 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl:32 \
     android.hardware.camera.provider@2.4-service \
-    android.hardware.camera.provider@2.5 \
+    android.hardware.camera.provider@2.5:64 \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    android.hardware.camera.device@3.4.so \
-    libdng_sdk.vend \
+    android.hardware.camera.device@3.4:64 \
+    libdng_sdk.vendor \
     Snap
 
 # Bluetooth
