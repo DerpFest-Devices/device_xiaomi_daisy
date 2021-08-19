f307f14419a91833acfe627ffbdd6a42a9dd039b
Hotorou <zunayeddihan@gmail.com>
2021-08-19 02:18:30 +0000

    Revert "daisy: Use 64bit dex2oat on all cores & daisy: Optimize ART"
    This reverts commit 4cb9fe808cf9ffed995a85e1500587721794a1d2, 406d81f5e3a89b7f3cc38090a4a7ebef069f7547

--- prop.mk
+++ prop.mk
@@ -93,19 +93,6 @@ ro.charger.enable_suspend=true
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.debug.coresight.config=stm-events
 
-# Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-dalvik.vm.dex2oat64.enabled=true \
-ro.sys.fw.dex2oat_thread_count=8 \
-dalvik.vm.image-dex2oat-filter=quicken \
-dalvik.vm.image-dex2oat-threads=8 \
-dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
-dalvik.vm.dex2oat-filter=quicken \
-dalvik.vm.dex2oat-threads=8 \
-dalvik.vm.dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
-dalvik.vm.boot-dex2oat-threads=8 \
-dalvik.vm.boot-dex2oat-cpu-set=0,1,2,3,4,5,6,7
-
 # Disable-Blur
 PRODUCT_PRODUCT_PROPERTIES += \
 persist.sys.sf.disable_blurs=1
