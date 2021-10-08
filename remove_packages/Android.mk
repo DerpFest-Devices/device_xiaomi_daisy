ea73d9ac0fc3a03a24a9d611d8ccf0686b07d1a3
Hotorou <zunayeddihan@gmail.com>
2021-10-08 08:56:15 +0000

    daisy: switch to google recorder instead of oneplus one
    Signed-off-by: ZunayedDihan <zunayeddihan@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := MaestroPrebuilt FilesPrebuilt ScribePrebuilt SoundAmplifierPrebuilt Stk arcore SafetyHubPrebuilt GoogleFeedback TipsPrebuilt AppDirectedSMSService CarrierSetup ConnMO DCMO Drive Gallery2 FM2 MyVerizonServices OBDM_Permissions OemDmTrigger PixelWallpapers2020 PixelLiveWallpaperPrebuilt PrebuiltGmail Showcase SprintDM SprintHM USCCDM talkback VZWAPNLib VzwOmaTrigger Maps Photos YouTube YouTubeMusicPrebuilt libqcomfm_jni obdm_stub TagGoogle PixelSetupWizard
+LOCAL_OVERRIDES_PACKAGES := MaestroPrebuilt FilesPrebuilt ScribePrebuilt SoundAmplifierPrebuilt Stk arcore SafetyHubPrebuilt GoogleFeedback TipsPrebuilt AppDirectedSMSService CarrierSetup ConnMO DCMO Drive Gallery2 FM2 MyVerizonServices OBDM_Permissions OemDmTrigger PixelWallpapers2020 PixelLiveWallpaperPrebuilt PrebuiltGmail Showcase SprintDM SprintHM USCCDM talkback VZWAPNLib VzwOmaTrigger Maps Photos YouTube YouTubeMusicPrebuilt libqcomfm_jni obdm_stub TagGoogle PixelSetupWizard OPScreenRecord
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
