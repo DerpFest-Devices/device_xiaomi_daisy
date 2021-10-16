687a1e81f2c81d4a13e0ba6573c2e1ccede90cdf
GeoPD <geoemmanuelpd2001@gmail.com>
2021-10-17 02:19:03 +0530

    daisy: Force disable iorapd
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- device.mk
+++ device.mk
@@ -590,6 +590,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     WfdCommon
 
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.iorapd.enable=false
+
 # Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
