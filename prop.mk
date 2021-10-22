b8c1a2af92aea2e8d0a57d9e2f5362f5bb951706
GeoPD <geoemmanuelpd2001@gmail.com>
2021-10-22 09:23:49 +0530

    daisy: Disable camera perflock
    ->Seems to improve the HDR processing in Gcam a bit fast.
      and we don't have any boostframework binaries.
    
    Signed-off-by: GeoPD <geoemmanuelpd2001@gmail.com>

--- prop.mk
+++ prop.mk
@@ -80,6 +80,7 @@ persist.vendor.camera.gyro.disable=0 \
 persist.vendor.camera.isp.clock.optmz=0 \
 persist.vendor.camera.stats.test=5 \
 persist.vendor.camera.CDS=off \
+persist.vendor.camera.perflock.enable=0 \
 persist.vendor.camera.HAL3.enabled=1
 
 # Cne
