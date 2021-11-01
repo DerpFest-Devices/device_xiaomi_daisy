6b4a16dfab0315f41a3bceff8a40074eb885711c
Kyle Harrison <khwebmail@gmail.com>
2021-11-01 11:58:13 +0530

    daisy: props: Don't write binary XML files
    - Android 12 now writes XML files in binary format by default. This can cause incompatibility with TWRP which can hang when attempting to read XML files e.g. /data/system/storage.xml
    
    - This commit sets the persist.sys.binary_xml property to false so that XML files are written in text format.
    
    Change-Id: If6430597d8459e62dd78aa458c58237c71618970
    Signed-off-by: ZunayedDihan <zunayeddihan@gmail.com>

--- device.mk
+++ device.mk
@@ -614,6 +614,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.iorapd.enable=false
 
+# System
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.binary_xml=false
+
 # Speed profile services and wifi-service to reduce RAM and storage.
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
 
