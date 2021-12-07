9554385cb80117e40c1e84c6fcc3ccecd97087f3
ZunayedDihan <zunayeddihan@gmail.com>
2021-12-08 00:37:51 +0600

    daisy: update removed package list
    Signed-off-by: ZunayedDihan <zunayeddihan@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := MaestroPrebuilt FilesPrebuilt ScribePrebuilt SoundAmplifierPrebuilt Stk arcore SafetyHubPrebuilt GoogleFeedback TipsPrebuilt AppDirectedSMSService CarrierSetup ConnMO DCMO Drive Gallery2 FM2 MyVerizonServices OBDM_Permissions OemDmTrigger PixelWallpapers2020 PixelLiveWallpaperPrebuilt PrebuiltGmail Showcase SprintDM SprintHM USCCDM talkback VZWAPNLib VzwOmaTrigger Maps Photos YouTube YouTubeMusicPrebuilt libqcomfm_jni obdm_stub TagGoogle PixelSetupWizard OPScreenRecord
+LOCAL_OVERRIDES_PACKAGES := MaestroPrebuilt ScribePrebuilt Stk arcore SafetyHubPrebuilt GoogleFeedback TipsPrebuilt AppDirectedSMSService CarrierSetup ConnMO DCMO Drive FM2 MyVerizonServices OemDmTrigger PixelWallpapers2020 PixelLiveWallpaperPrebuilt Showcase SprintDM SprintHM USCCDM talkback VZWAPNLib VzwOmaTrigger Maps YouTube YouTubeMusicPrebuilt libqcomfm_jni obdm_stub TagGoogle PixelSetupWizard OPScreenRecord
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
