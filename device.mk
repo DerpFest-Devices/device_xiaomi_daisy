71f5c66b68711d4c9aa9c340166398e28abbf4f6
ZVNexus <akoskovich@pm.me>
2022-08-04 05:43:46 +0600

    daisy: Remove vendor RenderScript implementation
     * RenderScript is deprecated on newer platforms and
       is being officially replaced.
    
     * On April 19, 2021, Google announced that RenderScript
       will be deprecated in Android 12, and recommended
       porting existing code to Vulkan.
    
       https://android-developers.googleblog.com/2021/04/android-gpu-compute-going-forward.html
    
    Signed-off-by: LinkBoi00 <linkdevel@protonmail.com>

--- device.mk
+++ device.mk
@@ -472,10 +472,6 @@ PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
 
-# RenderScript HAL
-PRODUCT_PACKAGES += \
-    android.hardware.renderscript@1.0-impl
-
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
