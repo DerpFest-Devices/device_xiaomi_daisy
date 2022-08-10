79c13750673cf5802d650fe3a8df1ec442fd4352
ZunayedDihan <zunayeddihan@gmail.com>
2022-08-10 17:42:42 +0600

    Revert "daisy: Remove vendor RenderScript implementation"
    This reverts commit 71f5c66b68711d4c9aa9c340166398e28abbf4f6.

--- device.mk
+++ device.mk
@@ -472,6 +472,10 @@ PRODUCT_PACKAGES += \
     PresencePolling \
     RcsService
 
+# RenderScript HAL
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
